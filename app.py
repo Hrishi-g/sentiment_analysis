c1a4380d438da6df622634e0998b5ca015e9939f
Hrishi-g <101734211+Hrishi-g@users.noreply.github.com>
2023-04-20 07:56:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ import base64
 #         output_sentiment = "Neutral"
         
 #     st.write("The input text is classified as:", output_sentiment)
-st.title("Text Classification with Random Forests")
-input_text = st.text_input("Enter some text to classify:")
+st.title("Social media sentimemt analysis")
+input_text = st.text_input("Enter some text:")
 sentiment = SentimentAnalysis(input_text)
 
 
@@ -34,15 +34,15 @@ def get_emoji(sentiment):
     
 font_size = "60px"
 
-def get_audio(score):
-    if sentiment == "Positive":
-        return "music/happy.mp3"
-    elif sentiment == "Negative":
-        return "music/sad.mp3"
+#def get_audio(score):
+   # if sentiment == "Positive":
+   #     return "music/happy.mp3"
+   # elif sentiment == "Negative":
+   #     return "music/sad.mp3"
     # elif score > 0:
     #     return "calm_audio.mp3"
-    else:
-        return "music/sad.mp3"
+    #else:
+       # return "music/sad.mp3"
     
 # def autoplay_audio(file_path: str):
 #     with open(file_path, "rb") as f:
@@ -62,10 +62,10 @@ if input_text:
     sentiment = SentimentAnalysis(input_text)
     st.write(f'<span style="font-size:{font_size}">{get_emoji(sentiment)}</span>',  unsafe_allow_html=True)
     
-    audio_file = open(get_audio(sentiment), "rb")
+    #audio_file = open(get_audio(sentiment), "rb")
     #autoplay_audio(audio_file)
-    audio_bytes = audio_file.read()
+    #audio_bytes = audio_file.read()
     #st.audio(audio_bytes, format="audio/mp3")
-    audio_b64 = base64.b64encode(audio_bytes).decode("utf-8")
+    #audio_b64 = base64.b64encode(audio_bytes).decode("utf-8")
     #b64 = base64.b64encode(data).decode()
-    st.markdown(f'<audio controls="controls"><source src="data:audio/mp3;base64,{audio_b64}" type="audio/mp3" /></audio>', unsafe_allow_html=True)
+    #st.markdown(f'<audio controls="controls"><source src="data:audio/mp3;base64,{audio_b64}" type="audio/mp3" /></audio>', unsafe_allow_html=True)
